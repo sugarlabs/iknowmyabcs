d66ff78f20d26b259f09cf48302545e92552b2cd
Yash Agrawal <yagrawal900@gmail.com>
2018-05-19 15:21:03 +0530

    port setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if len(sys.argv) > 1 and '--no-sugar' == sys.argv[1]:
            data_files = DATA_FILES,
            )
 else: 
-    from sugar.activity import bundlebuilder
+    from sugar3.activity import bundlebuilder
 
     if __name__ == "__main__":
         bundlebuilder.start()
